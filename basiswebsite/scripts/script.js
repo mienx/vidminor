2de399e9e243422311c3b116b97d57e4ac6e1720
mienx <118361398+mienx@users.noreply.github.com>
2023-11-08 16:00:04 +0100

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -2,7 +2,7 @@
 // console.log("Howdy!");
 
 // define all variables
-var waterButton = document.querySelector("img:nth-of-type(1)");
+var waterButton = document.querySelector("section img:nth-of-type(1)");
 var Dialog1 = document.querySelector("dialog:nth-of-type(1)");
 var aarderijk = document.querySelector("img:nth-of-type(2)");
 var Dialog2 = document.querySelector("dialog:nth-of-type(2)");
@@ -10,6 +10,10 @@ var luchtrijk = document.querySelector("img:nth-of-type(3)");
 var Dialog3 = document.querySelector("dialog:nth-of-type(3)");
 var Vuurnatie = document.querySelector("img:nth-of-type(4)");
 var Dialog4 = document.querySelector("dialog:nth-of-type(4)");
+
+// achtergrond liedje
+var musicButton = document.querySelector(".muteButton");
+var achtergrondmMuziek = new Audio('./images/avatarliedje.mp3');
 
 // console.log(waterButton);
 
@@ -35,3 +39,17 @@ function showAapInfo3() {
 function showAapInfo4() {
 	Dialog4.showModal();
 }
+
+
+
+function muteMusicPlayer() {
+    if (achtergrondmMuziek.paused) {
+        achtergrondmMuziek.play();
+        musicButton.src = "./images/music-note-on-2.png"
+    } else {
+        achtergrondmMuziek.pause();
+        musicButton.src = "./images/music-note-2.png"
+
+    }
+}
+musicButton.addEventListener("click", muteMusicPlayer);
